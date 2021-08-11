c9b440bcd2ef1f3dfb42a175d2a2201cd54e8774
Lucho <lmencos@hotmail.com>
2021-08-11 15:35:32 -0500

    Starting lucho stickers app

---
+++ src/StickersLuchoApp.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const StickersLuchoApp = () => {
+  return (
+    <>
+      <h4>Stickers Lucho App</h4>
+      <hr />
+      <input type="text" />
+    </>
+  )
+}
